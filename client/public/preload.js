a737e23f9ad3e28c2b4c668a76b0516151587206
KGROLLEMUND <kyllian.grollemund@gmail.com>
2023-03-20 09:19:31 +0100

    push

--- client/public/preload.js
+++ client/public/preload.js
@@ -1,7 +1,17 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const { contextBridge } = require("electron");
+const fs = require("fs");
+const path = require("path");
 
+// Expose the `fs` and `path` modules to the renderer process
+contextBridge.exposeInMainWorld("fs", {
+  readFileSync: (path) => {
+    return fs.readFileSync(path);
+  },
+});
+
+contextBridge.exposeInMainWorld("path", path);
 // As an example, here we use the exposeInMainWorld API to expose the browsers
 // and node versions to the main window.
 // They'll be accessible at "window.versions".
